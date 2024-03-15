746cc75d0fa16e52e056f0dd28507f8bb9043deb
HoutSovannarith <hout.sovannarith2000@gmail.com>
2024-03-15 19:11:12 +0700

    Update value on option in form

--- app/enroll/page.tsx
+++ app/enroll/page.tsx
@@ -42,9 +42,32 @@ export default function Enroll() {
                                 <span className="text-red-500">*</span>
                             </div>
                             <Select id="course" required>
-                                <option disabled selected>Select place of birth</option>
-                                <option>Male</option>
-                                <option>Female</option>
+                                <option value="0" disabled selected>Select province</option>
+                                <option value="1">Banteay Meanchey</option>
+                                <option value="2">Battambang</option>
+                                <option value="3">Kampong Cham</option>
+                                <option value="4">Kampong Chhnang</option>
+                                <option value="5">Kampong Speu</option>
+                                <option value="6">Kampong Thom</option>
+                                <option value="7">Kampot</option>
+                                <option value="8">Kandal</option>
+                                <option value="9">Koh Kong</option>
+                                <option value="10">Kratie</option>
+                                <option value="11">Mondul Kiri</option>
+                                <option value="12">Phnom Penh</option>
+                                <option value="13">Preah Vihear</option>
+                                <option value="14">Prey Veng</option>
+                                <option value="15">Pursat</option>
+                                <option value="16">Ratanak Kiri</option>
+                                <option value="17">Siemreap</option>
+                                <option value="18">Preah Sihanouk</option>
+                                <option value="19">Stung Treng</option>
+                                <option value="20">Svay Rieng</option>
+                                <option value="21">Takeo</option>
+                                <option value="22">Oddar Meanchey</option>
+                                <option value="23">Kep</option>
+                                <option value="24">Pailin</option>
+                                <option value="25">Tboung Khmum</option>
                             </Select>
                         </div>
                         <div>
@@ -53,9 +76,21 @@ export default function Enroll() {
                                 <span className="text-red-500">*</span>
                             </div>
                             <Select id="course" required>
-                                <option disabled selected>Select current address</option>
-                                <option>Male</option>
-                                <option>Female</option>
+                                <option value="0" disabled selected>Select current address</option>
+                                <option value="1">Chamkar Mon</option>
+                                <option value="2">Doun Penh</option>
+                                <option value="3">Prampir Meakkakra</option>
+                                <option value="4">Tuol Kouk</option>
+                                <option value="5">Dangkao</option>
+                                <option value="6">Mean Chey</option>
+                                <option value="7">Russey Keo</option>
+                                <option value="8">Saensokh</option>
+                                <option value="9">Pur SenChey</option>
+                                <option value="10">Chrouy Changvar</option>
+                                <option value="11">Praek Pnov</option>
+                                <option value="12">Chbar Ampov</option>
+                                <option value="13">Boeng Keng Kang</option>
+                                <option value="14">Kamboul</option>
                             </Select>
                         </div>
                     </form>
@@ -83,9 +118,12 @@ export default function Enroll() {
                                 <span className="text-red-500">*</span>
                             </div>
                             <Select id="course" required>
-                                <option disabled selected>Select education</option>
-                                <option>Male</option>
-                                <option>Female</option>
+                                <option value="0" disabled selected>Select education</option>
+                                <option value="1">First Year</option>
+                                <option value="2">Second Year</option>
+                                <option value="2">Third Year</option>
+                                <option value="2">Fourth Year</option>
+                                <option value="2">Graduated</option>
                             </Select>
                         </div>
                         <div>
@@ -94,9 +132,50 @@ export default function Enroll() {
                                 <span className="text-red-500">*</span>
                             </div>
                             <Select id="course" required>
-                                <option disabled selected>Select university</option>
-                                <option>Male</option>
-                                <option>Female</option>
+                                <option value="0" disabled selected>Select university</option>
+                                <option value="1">ACLEDA Institute of Business</option>
+                                <option value="2">American University of Phnom Penh</option>
+                                <option value="3">Asia Euro University</option>
+                                <option value="4">Beltei International University</option>
+                                <option value="5">Build Bright University</option>
+                                <option value="42">Cambodia Academy of Digital Technology</option>
+                                <option value="7">Cambodian Mekong University</option>
+                                <option value="6">Cambodia University for Specialties</option>
+                                <option value="8">CamEd Business School</option>
+                                <option value="9">Chenla University</option>
+                                <option value="10">East Asia Management University</option>
+                                <option value="11">Economics and Finance Institute</option>
+                                <option value="12">Human Resources University</option>
+                                <option value="13">IIC University of Technology</option>
+                                <option value="14">Institute of Technology of Cambodia</option>
+                                <option value="15">International University</option>
+                                <option value="43">IT Academy STEP Cambodia</option>
+                                <option value="16">Khemarak University</option>
+                                <option value="17">Limkokwing University of Creative Technology, Cambodia</option>
+                                <option value="18">National Institute of Business</option>
+                                <option value="19">National Institute of Education</option>
+                                <option value="20">National Polytechnic Institute of Cambodia</option>
+                                <option value="21">National University of Management</option>
+                                <option value="22">Norton University</option>
+                                <option value="40">Other</option>
+                                <option value="23">Panha Chiet University</option>
+                                <option value="24">Paññasastra University of Cambodia</option>
+                                <option value="25">Paragon International University</option>
+                                <option value="26">Phnom Penh International University</option>
+                                <option value="27">Preah Sihanouk Raja Buddhist University</option>
+                                <option value="28">Prek Leap National College of Agriculture</option>
+                                <option value="29">Royal Academy of Cambodia</option>
+                                <option value="30">Royal University of Agriculture</option>
+                                <option value="31">Royal University of Fine Arts</option>
+                                <option value="32">Royal University of Law and Economics</option>
+                                <option value="33">Royal University of Phnom Penh</option>
+                                <option value="41">Setec Institute</option>
+                                <option value="34">The University of Cambodia</option>
+                                <option value="35">University of Battambang</option>
+                                <option value="36">University of Management and Economics</option>
+                                <option value="37">University of Puthisastra</option>
+                                <option value="38">Vanda Institute</option>
+                                <option value="39">Western University Cambodia</option>
                             </Select>
                         </div>
                     </form>
@@ -114,21 +193,21 @@ export default function Enroll() {
                         <span className="text-red-500">*</span>
                     </div>
                     <Select id="course" required>
-                        <option disabled selected>Select Course</option>
-                        <option>Flutter Mobile Development</option>
-                        <option>iOS Development</option>
-                        <option>SQL & Data Modeling with PostgreSQL</option>
-                        <option>Web Design</option>
-                        <option>DevOps Engineering</option>
-                        <option>C++ Programming</option>
-                        <option>Docker</option>
-                        <option>Data Analytics</option>
-                        <option>Android Development</option>
-                        <option>NEXT.js</option>
-                        <option>Linux Administrator</option>
-                        <option>Spring Framework</option>
-                        <option>Blockchain Development</option>
-                        <option>Java Programming</option>
+                        <option value="0" disabled selected>Select Course</option>
+                        <option value="1">Flutter Mobile Development</option>
+                        <option value="2">iOS Development</option>
+                        <option value="3">SQL & Data Modeling with PostgreSQL</option>
+                        <option value="4">Web Design</option>
+                        <option value="5">DevOps Engineering</option>
+                        <option value="6">C++ Programming</option>
+                        <option value="7">Docker</option>
+                        <option value="8">Data Analytics</option>
+                        <option value="9">Android Development</option>
+                        <option value="10">NEXT.js</option>
+                        <option value="11">Linux Administrator</option>
+                        <option value="12">Spring Framework</option>
+                        <option value="13">Blockchain Development</option>
+                        <option value="14">Java Programming</option>
                     </Select>
                 </div>
                 <div className="py-2 text-red-500 font-bold">
